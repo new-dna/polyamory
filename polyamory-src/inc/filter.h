be5cedde49b0d9b5b863f4ac4e2b87476ba3679c
Nick McCarty <mccartyn@gmail.com>
2017-10-06 12:47:18 -0700

    Added credit for the source of the simple saw filter. Modified the code to mix (add) the two saws for a more interesting demo

--- polyamory-src/inc/filter.h
+++ polyamory-src/inc/filter.h
@@ -1,3 +1,15 @@
+/*
+ * From:
+ * //****************************************************
+//
+//   EXAMPLE OF USING CS42L22 on STM32F4-Discovery
+//
+//   author: A.Finkelmeyer
+//   http://www.mind-dump.net/configuring-the-stm32f4-discovery-for-audio
+//
+//****************************************************
+ */
+
 //Includes
 #include <stdio.h>
 
@@ -5,9 +17,9 @@
 //Defines
 
 
-#define NOTEFREQUENCY 0.0158		//frequency of saw wave: f0 = 0.5 * NOTEFREQUENCY * 48000 (=sample rate)
-#define NOTEFREQUENCY2 0.0458
-#define NOTEAMPLITUDE 750.0		//amplitude of the saw wave
+#define NOTEFREQUENCY 0.01		//frequency of saw wave: f0 = 0.5 * NOTEFREQUENCY * 48000 (=sample rate)
+#define NOTEFREQUENCY2 0.012
+#define NOTEAMPLITUDE 800.0		//amplitude of the saw wave
 
 
 typedef struct {
